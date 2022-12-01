8f38bd272bf50113d82417483d2c64df4f2d1e77
lip2d <filipe258anacleto@hotmail.com>
2022-12-01 18:39:37 -0300

    aula 19

--- livraria/settings.py
+++ livraria/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'core',
     "rest_framework_simplejwt",
     "uploader",
+    "drf_spectacular",
     'rest_framework',
 ]
 
@@ -49,6 +50,13 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Livraria API",
+    "DESCRIPTION": "API para gerenciamento de livraria, incluindo endpoints e documentação.",
+    "VERSION": "1.0.0",
 }
 
 ROOT_URLCONF = 'livraria.urls'
