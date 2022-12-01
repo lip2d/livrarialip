d22b6b8996339ee291810bbcd2cdc60e4da3f197
lip2d <filipe258anacleto@hotmail.com>
2022-12-01 18:19:02 -0300

    aula 13

--- livraria/settings.py
+++ livraria/settings.py
@@ -40,6 +40,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissions",
+    ],
+}
+
 ROOT_URLCONF = 'livraria.urls'
 
 TEMPLATES = [
